08dc8ca16a67fb4ba7fbc5a48639080e25cbb815
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-30 16:40:16 -0600

    Add complete snail method with arrays + accumulator input

--- snail_challenge.rb
+++ snail_challenge.rb
@@ -6,11 +6,44 @@
 # continutes this process until everything is complete.
 # recursion makes sense, as does usage of pop/shift/push
 
+def snail(arrays, acc = [])
+  unless arrays == []
+    arrays.first.each do |num|
+      acc.push(num)
+    end
+    arrays.shift
+  end
+  unless arrays == []
+    arrays.map do |array|
+      acc.push(array.pop)
+    end
+    until arrays.last == []
+      acc.push(arrays.last.pop)
+    end
+    arrays.pop
+    arrays.reverse.map do |array|
+      acc.push(array.shift)
+    end
+    snail(arrays, acc)
+  end
+  return acc
+end
 
-arrayMatrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
+arrays1 = [
+  [1,2,3,4],
+  [5,6,7,8],
+  [9,10,11,12],
+  [13,14,15,16]
+]
 
-puts snail(arrayMatrix)
+arrays2 = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25]
+]
+
+puts snail(arrays1)
+puts '------------'
+puts snail(arrays2)
