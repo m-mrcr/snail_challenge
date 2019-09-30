b54aa7024cd51657b3f176ee1f7aaeee6b560d32
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-09-30 15:10:49 -0600

    Add thoughts on structure of function

--- snail_challenge.rb
+++ snail_challenge.rb
@@ -1,3 +1,12 @@
+# Ideation:
+# method must start from top, reading left to right
+# then continues down through the right-most digits as in a column
+# then proceeds through bottom digits, reading right to left
+# then moves up through the left-most digits, again as in a column
+# continutes this process until everything is complete.
+# recursion makes sense, as does usage of pop/shift/push
+
+
 arrayMatrix = [
   [1, 2, 3],
   [4, 5, 6],
